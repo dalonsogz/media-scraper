b25b53f255c99e784bfd77eddd4a750a1d160679
dalonsogz <dalonsogz@yahoo.es>
2023-12-16 20:08:48 +0100

    Updated version to steam project improvements.

--- web_scraper.py
+++ web_scraper.py
@@ -8,25 +8,24 @@ import urllib3
 import re
 import json
 import os
-from itemSteamStore import SteamStoreConfig as siteCfg, SteamStoreItem as siteItem, SteamStoreMethods as siteMethods, SteamStoreMongo as siteDB
+from itemTheGamesdb import TheGamesDBConfig as siteCfg, TheGamesDBItem as siteItem, TheGamesDBMethods as siteMethods, TheGamesDBMongo as siteDB
 from bs4 import BeautifulSoup
 
 # Debug flag to disable adding data to database
-flagAddToDatabase = True
+addToDatabase = True
 # If the item index exists in database do not download
-notDownloadIfExists = True
+downloadIfExists = False
 # If the item index exists do not add it to database
-notAddIfExists = True
+addIfExists = False
 
 
 class RequestFiles:
-
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
-    procesedItemsFile = "procesedItems.txt"
+    processedItemsFile = "processedItems_{}.txt"
     maxTimeBtwRequest = 2
     items = []
-    procesedItems = []
+    processedItems = []
     notProcessedItems = []
 
     # Site parameters
@@ -42,21 +41,21 @@ class RequestFiles:
     ########################## D O W N L O A D    I T E M S    S E C T I O N
 
     def read_items_processed(self):
-        procesedItems = []
+        processedItems = []
         try:
-            with open(self.procesedItemsFile, 'r', encoding='utf8') as f:
+            with open(self.processedItemsFile, 'r', encoding='utf8') as f:
                 for line in f:
                     try:
-                        procesedItems.append(int(line.strip()))
+                        processedItems.append(int(line.strip()))
                     except ValueError:
                         pass
-                # procesedItems = list(lines.split(" "))
+                # processedItems = list(lines.split(" "))
         except FileNotFoundError:
-            logger.error("Creating new '{}' file".format(self.procesedItemsFile))
-        return procesedItems
+            logger.error("Creating new '{}' file".format(self.processedItemsFile))
+        return processedItems
 
     def write_item_processed(self, i):
-        with open(self.procesedItemsFile, 'a', encoding='utf8') as f:
+        with open(self.processedItemsFile, 'a', encoding='utf8') as f:
             f.write("{}\n".format(str(i)))
 
     def common_requests(self, urlBase, i):
@@ -76,8 +75,10 @@ class RequestFiles:
 
     def downloadItems(self, item, downloadfrom, downloadto):
         totalItems = list(range(downloadfrom, downloadto + 1))
-        self.procesedItems = self.read_items_processed()
-        self.notProcessedItems = list(set(totalItems) - set(self.procesedItems))
+        self.processedItems = self.read_items_processed()
+        self.notProcessedItems = list(set(totalItems))
+        if not downloadIfExists:
+            self.notProcessedItems = list(set(totalItems) - set(self.processedItems))
 
         condition = len(self.notProcessedItems) > 0
         while condition:
@@ -105,15 +106,16 @@ def init_logger(baseName):
     with open('config.yml', 'r') as f:
         config = yaml.safe_load(f.read())
         file_name, extension = os.path.splitext(config['handlers']['file']['filename'])
-        config['handlers']['file']['filename']=f"{file_name}_{baseName}{extension}"
+        config['handlers']['file']['filename'] = f"{file_name}_{baseName}{extension}"
         logging.config.dictConfig(config)
-    return logging.getLogger()  # "web_scraper")
+    return logging.getLogger()
 
 
 def init_config():
     requestFiles = RequestFiles()
     (requestFiles.totalNumItems, requestFiles.baseName, requestFiles.urlBase,
      requestFiles.itemDataFilePattern, requestFiles.destFilePath, requestFiles.mongoCol) = siteCfg().config()
+    requestFiles.processedItemsFile = requestFiles.processedItemsFile.format(requestFiles.baseName)
     return requestFiles
 
 
@@ -121,11 +123,17 @@ def parseItemFilesAndAddToDabase(requestFiles, parseFrom, parseTo):
     fails = []
     for itemIndex in range(parseFrom, parseTo + 1):
         try:
-            item = requestFiles.parseHtml(itemIndex)
-            requestFiles.items.append(item)
-            jsonResult = siteMethods.toJson(item)
-            logger.info("\n---------\n{}\n---------\n".format(json.dumps(jsonResult, sort_keys=True, indent=4)))
-            requestFiles.mongoCol.insert_one(jsonResult)
+            results = list()
+            if not addIfExists:
+                results = list(requestFiles.mongoCol.find({"id": itemIndex}))
+
+            if len(results) == 0:
+                item = requestFiles.parseHtml(itemIndex)
+                requestFiles.items.append(item)
+                jsonResult = siteMethods.toJson(item)
+                logger.info("\n---------\n{}\n---------\n".format(json.dumps(jsonResult, sort_keys=True, indent=4)))
+                requestFiles.mongoCol.insert_one(jsonResult)
+
         except FileNotFoundError as err:
             logger.warning(f"{itemIndex}\tNot found {err=}, {type(err)=}")
             fails.append(itemIndex)
@@ -137,24 +145,26 @@ def parseItemFilesAndAddToDabase(requestFiles, parseFrom, parseTo):
             logger.error("Exception occurred", exc_info=True)
             fails.append(itemIndex)
 
-    logger.warning("fails({}):{}".format(len(fails), fails))
+    if len(fails) > 0:
+        logger.warning("fails({}):{}".format(len(fails), fails))
 
-def dowloadAndParseItems(parseFrom,parseTo):
+
+def dowloadAndParseItems(parseFrom, parseTo):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     requestFiles = init_config()
     global logger
     logger = init_logger(requestFiles.baseName)
 
     # Dowload web data
-    requestFiles.downloadItems(siteItem(),parseFrom, parseTo)
+    requestFiles.downloadItems(siteItem(), parseFrom, parseTo)
 
     # Parse web data and add to database
-    if (parseTo-parseFrom)>=0 and flagAddToDatabase:
+    if (parseTo - parseFrom) >= 0 and addToDatabase:
         parseItemFilesAndAddToDabase(requestFiles, parseFrom, parseTo)
 
 def main():
     # Indexes between to start and stop downloading
-    processFrom,processTo = 1174180, 1174180
+    processFrom,processTo = 120880, 121572
     dowloadAndParseItems(processFrom,processTo)
 
 if __name__ == "__main__":

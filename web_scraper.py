6f18a5397f814efa356e22452abea501294c9a5e
dalonsogz <dalonsogz@yahoo.es>
2024-06-08 23:21:57 +0200

    Regular update

--- web_scraper.py
+++ web_scraper.py
@@ -164,7 +164,7 @@ def dowloadAndParseItems(parseFrom, parseTo):
 
 def main():
     # Indexes between to start and stop downloading
-    processFrom,processTo = 124600,125963
+    processFrom,processTo = 125964,127108
     dowloadAndParseItems(processFrom,processTo)
 
 if __name__ == "__main__":
